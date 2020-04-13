8ba022001ee7dcac3da96696614dfff132d3c5ed
Fides Escalona <fescalonamolina@gmail.com>
2020-04-13 13:14:41 -0500

    Updates

---
+++ precache-manifest.fc98a3c8b8b1f97b647c345bb53f25ba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-js/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "4cb392830847bad0ed48",
+    "url": "/react-js/static/css/main.5fddd905.chunk.css"
+  },
+  {
+    "revision": "6c59e033da9f12c865a1",
+    "url": "/react-js/static/js/runtime~main.6c59e033.js"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-js/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f44ed3fac3ceed93815a",
+    "url": "/react-js/static/js/2.f44ed3fa.chunk.js"
+  },
+  {
+    "revision": "4cb392830847bad0ed48",
+    "url": "/react-js/static/js/main.4cb39283.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-js/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-js/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-js/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "f44ed3fac3ceed93815a",
+    "url": "/react-js/static/css/2.5ecb8115.chunk.css"
+  },
+  {
+    "revision": "ba079fed59d0d6a4f8bfe8b6f4925b54",
+    "url": "/react-js/index.html"
+  }
+];
